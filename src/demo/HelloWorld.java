5d94bf3cab7b4d2b81c602d7feb928a324338530
Fardous kabir <kabirfardous143@gmail.com>
2023-02-05 13:28:04 -0500

    first commit

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("we are ready to start java programing");
+       System.out.println("fardous kabir");
+       System.out.println("");
+       
+	
+	
+	
+	}
+
+}
